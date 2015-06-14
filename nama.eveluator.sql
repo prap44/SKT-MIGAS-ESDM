76b90eddebc00b11263fe42062f1885be31ed5be
rezamukti <ycared@gmail.com>
2015-06-14 20:57:11 +0700

    ini data untuk eveluator

---
+++ nama.eveluator.sql
@@ -0,0 +1,78 @@
+INSERT INTO `skt_sosialisasi`.`users` (`username`, `password`, `status`, `level`, `nama_lengkap`) VALUES 
+('test1@gmail.com', '2e5ea3bff1f4ba7de3d72f28601d2e66 ', '1', '6', 'Sudarto');
+
+INSERT INTO `skt_sosialisasi`.`users` (`username`, `password`, `status`, `level`, `nama_lengkap`) VALUES 
+('test2@gmail.com', '2e5ea3bff1f4ba7de3d72f28601d2e66 ', '1', '6', 'Diah Indrarini');
+
+INSERT INTO `skt_sosialisasi`.`users` (`username`, `password`, `status`, `level`, `nama_lengkap`) VALUES 
+('test3@gmail.com', '2e5ea3bff1f4ba7de3d72f28601d2e66 ', '1', '6', 'Murtadho');
+
+INSERT INTO `skt_sosialisasi`.`users` (`username`, `password`, `status`, `level`, `nama_lengkap`) VALUES 
+('test4@gmail.com', '2e5ea3bff1f4ba7de3d72f28601d2e66 ', '1', '6', 'Evaningsih');
+
+
+INSERT INTO `skt_sosialisasi`.`users` (`username`, `password`, `status`, `level`, `nama_lengkap`) VALUES 
+('test5@gmail.com', '2e5ea3bff1f4ba7de3d72f28601d2e66 ', '1', '6', 'Bajonggan Silaban');
+
+
+INSERT INTO `skt_sosialisasi`.`users` (`username`, `password`, `status`, `level`, `nama_lengkap`) VALUES 
+('test6@gmail.com', '2e5ea3bff1f4ba7de3d72f28601d2e66 ', '1', '6', 'Sukarman');
+
+
+INSERT INTO `skt_sosialisasi`.`users` (`username`, `password`, `status`, `level`, `nama_lengkap`) VALUES 
+('test7@gmail.com', '2e5ea3bff1f4ba7de3d72f28601d2e66 ', '1', '6', 'suharti');
+
+
+INSERT INTO `skt_sosialisasi`.`users` (`username`, `password`, `status`, `level`, `nama_lengkap`) VALUES 
+('test7@gmail.com', '2e5ea3bff1f4ba7de3d72f28601d2e66 ', '1', '6', 'waris sambodo');
+
+
+INSERT INTO `skt_sosialisasi`.`users` (`username`, `password`, `status`, `level`, `nama_lengkap`) VALUES 
+('test8@gmail.com', '2e5ea3bff1f4ba7de3d72f28601d2e66 ', '1', '6', 'syafarudin');
+
+
+INSERT INTO `skt_sosialisasi`.`users` (`username`, `password`, `status`, `level`, `nama_lengkap`) VALUES 
+('test9@gmail.com', '2e5ea3bff1f4ba7de3d72f28601d2e66 ', '1', '6', 'kusriyanto');
+
+
+INSERT INTO `skt_sosialisasi`.`users` (`username`, `password`, `status`, `level`, `nama_lengkap`) VALUES 
+('test10@gmail.com', '2e5ea3bff1f4ba7de3d72f28601d2e66 ', '1', '6', 'anna miyosi');
+
+
+INSERT INTO `skt_sosialisasi`.`users` (`username`, `password`, `status`, `level`, `nama_lengkap`) VALUES 
+('test11@gmail.com', '2e5ea3bff1f4ba7de3d72f28601d2e66 ', '1', '6', 'alfianur');
+
+
+INSERT INTO `skt_sosialisasi`.`users` (`username`, `password`, `status`, `level`, `nama_lengkap`) VALUES 
+('test12@gmail.com', '2e5ea3bff1f4ba7de3d72f28601d2e66 ', '1', '6', 'riyanto');
+
+
+INSERT INTO `skt_sosialisasi`.`users` (`username`, `password`, `status`, `level`, `nama_lengkap`) VALUES 
+('test13@gmail.com', '2e5ea3bff1f4ba7de3d72f28601d2e66 ', '1', '6', 'syarif h');
+
+
+INSERT INTO `skt_sosialisasi`.`users` (`username`, `password`, `status`, `level`, `nama_lengkap`) VALUES 
+('test14@gmail.com', '2e5ea3bff1f4ba7de3d72f28601d2e66 ', '1', '6', 'agung catur');
+
+
+INSERT INTO `skt_sosialisasi`.`users` (`username`, `password`, `status`, `level`, `nama_lengkap`) VALUES 
+('test15@gmail.com', '2e5ea3bff1f4ba7de3d72f28601d2e66 ', '1', '6', 'bu linda');
+
+
+INSERT INTO `skt_sosialisasi`.`users` (`username`, `password`, `status`, `level`, `nama_lengkap`) VALUES 
+('test16@gmail.com', '2e5ea3bff1f4ba7de3d72f28601d2e66 ', '1', '6', 'nella riskiana');
+
+
+INSERT INTO `skt_sosialisasi`.`users` (`username`, `password`, `status`, `level`, `nama_lengkap`) VALUES 
+('test17@gmail.com', '2e5ea3bff1f4ba7de3d72f28601d2e66 ', '1', '6', 'revi adib');
+
+
+INSERT INTO `skt_sosialisasi`.`users` (`username`, `password`, `status`, `level`, `nama_lengkap`) VALUES 
+('test18@gmail.com', '2e5ea3bff1f4ba7de3d72f28601d2e66 ', '1', '6', 'rudy dwi');
+
+
+INSERT INTO `skt_sosialisasi`.`users` (`username`, `password`, `status`, `level`, `nama_lengkap`) VALUES 
+('test19@gmail.com', '2e5ea3bff1f4ba7de3d72f28601d2e66 ', '1', '6', 'riantina');
+
+
+
